a4fd3d7fd5361b83c9ac8e5bdc8c90ae5258e991
TusharSharma8201 <tusharsharma8201@gmail.com>
2021-09-26 13:10:31 +0530

    update

--- Basics.java
+++ Basics.java
@@ -1,13 +1,13 @@
 public class Basics{
 
-    public static void function(int a){
-        System.out.println(a);
+    public static int function(int a){
+       return a+10;
     }
 
     public static void main(String[] args){
         System.out.println("Hello");
         int a=20;
-        function(a);
+        System.out.println(function(a));
        
     }
 }
